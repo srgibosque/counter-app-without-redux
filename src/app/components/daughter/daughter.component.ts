bfe2805c02ee1bbd156c24157aa0e306b2c8dad7
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-04-04 21:16:35 +0200

    Daughter component controller and template

--- src/app/components/daughter/daughter.component.ts
+++ src/app/components/daughter/daughter.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-daughter',
   templateUrl: './daughter.component.html',
   styleUrl: './daughter.component.css'
 })
-export class DaughterComponent {
+export class DaughterComponent implements OnInit {
+  @Input() counter!: number;
+  @Output() counterChanged = new EventEmitter<number>();
 
+  constructor(){}
+
+  ngOnInit(): void {}
+
+  duplicate(): void{
+    this.counter = this.counter * 2;
+    this.counterChanged.emit(this.counter);
+  }
 }
